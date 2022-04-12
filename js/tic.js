4788caa03bd36475e513e137c696d0e8b4980c5c
Keville <Matthew.Keville@epscorp.com>
2022-04-12 16:48:41 -0400

    v0.3

--- js/tic.js
+++ js/tic.js
@@ -1,6 +1,4 @@
-
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera( 
   100,
   window.innerWidth / window.innerHeight,
@@ -13,15 +11,18 @@ renderer.setSize(
   window.innerWidth,
   window.innerHeight 
 );
+renderer.sortObjects = false;
 
 document.body.appendChild( renderer.domElement );
-
-let controls = new THREE.OrbitControls(camera,renderer.domElement);
 
 const light = new THREE.AmbientLight( 0x404040);
 scene.add(light);
 
+let controls = new THREE.OrbitControls(camera,renderer.domElement);
 
+///////////////////
+//draw unit vectors
+///////////////////
 
 var ipts = [];
 var jpts = [];
@@ -29,22 +30,20 @@ var kpts = [];
 
 ipts.push(
   new THREE.Vector3(0,0,0),
-  new THREE.Vector3(500,0,0)
+  new THREE.Vector3(3000,0,0)
 );
 
 jpts.push(
   new THREE.Vector3(0,0,0),
-  new THREE.Vector3(0,500,0)
+  new THREE.Vector3(0,3000,0)
 );
 
 kpts.push(
   new THREE.Vector3(0,0,0),
-  new THREE.Vector3(0,0,500)
+  new THREE.Vector3(0,0,3000)
 );
 
 
-
-//draw unit vectors
 var igeo = new THREE.BufferGeometry().setFromPoints( ipts );
 var jgeo = new THREE.BufferGeometry().setFromPoints( jpts );
 var kgeo = new THREE.BufferGeometry().setFromPoints( kpts );
@@ -56,35 +55,49 @@ scene.add(iline);
 scene.add(jline);
 scene.add(kline);
 
-//camera.translateX(200);
 camera.translateY(500);
-//camera.translateY(10);
-//camera.translateZ(500);
-//camera.translateZ(250);
-//camera.translateX(250);
 camera.lookAt(0,0);
-//camera.rotateOnWorldAxis( new THREE.Vector3( 0,1,0),-Math.PI/2);
+
+//////////////////////////////
+// construct the board space
+//////////////////////////////
 
 //spots must be perfect square
-let spr = 3;
-let spots = spr**2;
-let granularity = 5;
-let triangles = spots* (4**granularity) /2;
-//let vertices = triangles*3;
-//let dps = Math.sqrt(spots)*10;
-//let dps = Math.sqrt(triangles/2);
+let spr = 3; //spot base
+let spots = spr**2; //total spots ( a square number )
+let granularity = 7; //determine resolution of individual spots
+let triangles = spots* (4**granularity) /2; //triangles per spot
 
+//dimensions of spot space / (tic-toe) board
 
 let xmax = 400;
 let xmin = -xmax;
 let xrange = xmax - xmin;
-console.log(xrange);
-let zmax = 200;
+let zmax = 400;
 let zmin = -zmax;
 let zrange = zmax - zmin;
 
-let vf = [];
-let cf = [];
+let vf = []; //vertices     ( x,y,z,x,y,z,x,y ... )
+let cf = []; //color values ( r,g,b,r,g,b,r,g ... )
+
+//define X / O vertex data based on original state of board space
+/*
+//xGeometry will span the space of 1 face , minus the outside layer of it's granularity
+let xw = (xmax-xmin)/spr * (granularity-2)/granularity;
+let xh = (zmax-zmin)/spr * (granularity-2)/granularity;
+//let xGeometry = new THREE.PlaneGeometry(100,100,50);
+let xGeometry = new THREE.PlaneGeometry(xw,xh,50);
+
+let xMat = new THREE.MeshBasicMaterial( { color : 0xffff00 , side : THREE.DoubleSide});
+let xPlane = new THREE.Mesh( xGeometry , xMat );
+//rotate vertex set
+xPlane.geometry.rotateX(Math.PI/2);
+scene.add( xPlane );
+*/
+
+let xPlanes = [];
+
+
 
 //iterate over spaces in the board
 for ( let i = 0; i < spr; i++ ) {
@@ -102,7 +115,6 @@ for ( let i = 0; i < spr; i++ ) {
     let sfz = zmin + ((j+1)/spr)*(zmax-zmin);
 
     //sub-divide each spot by granularity fill
-    //
     //all sub regions of spot will be colored the same
         let r1 = Math.random();
         let g1 = Math.random();
@@ -121,7 +133,7 @@ for ( let i = 0; i < spr; i++ ) {
           r2,g2,b2,
           r2,g2,b2
         ] 
-    //
+
     for ( let a = 0; a < granularity; a++) {
       for ( let b = 0; b < granularity; b++) {
         //ranges of spot dimensions
@@ -145,74 +157,41 @@ for ( let i = 0; i < spr; i++ ) {
 
         vf.push(...vdata);
         cf.push(...cdata);
-    }
-    }
-  }
-}
-
-/*
-for ( let i = 0; i < dps; i++ ) {
-  for ( let j = 0; j < dps; j++ ) {
-
-    //top left
-    let x0 = xmin + (i/dps)*(xmax-xmin);
-    let z0 = zmin + (j/dps)*(zmax-zmin);
-
-    let x1 = xmin + ((i+1)/dps)*(xmax - xmin);
-    let z1 = zmin + ((j+1)/dps)*(zmax - zmin);
-
-    let vdata = [
-      x0,0,z0,
-      x1,0,z0,
-      x1,0,z1,
-      x1,0,z1,
-      x0,0,z1,
-      x0,0,z0
-    ]  
-
-    vf.push(...vdata);
-
-
-    let r1 = Math.random();
-    let g1 = Math.random();
-    let b1 = Math.random();
-
-    let r2 = Math.random();
-    let g2 = Math.random();
-    let b2 = Math.random();
-
-
-    let cdata = [
-      r1,g1,b1,
-      r1,g1,b1,
-      r1,g1,b1,
-      r2,g2,b2,
-      r2,g2,b2,
-      r2,g2,b2
-    ] 
-
  
-    cf.push(...cdata);
+        //insert X peices at the second layer of the
+        //face partition
+        if ( a == 1 && b == 1 ) {
+          //xGeometry will span the space of 1 face , minus the outside layer of it's granularity
+          let xw = (xmax-xmin)/spr * (granularity-2)/granularity;
+          let xh = (zmax-zmin)/spr * (granularity-2)/granularity;
+          //let xGeometry = new THREE.PlaneGeometry(xw,xh,10);
+          let xGeometry = new THREE.BoxGeometry(xw,xh,20,20,20,20);
+          let xMat = new THREE.MeshBasicMaterial( { color : 0xffff00 , side : THREE.DoubleSide});
+          let xPlane = new THREE.Mesh( xGeometry , xMat );
+          //rotate vertex set
+          xPlane.geometry.rotateX(Math.PI/2);
+          //translate X so that it is centered in it's corresponding face
+          xPlane.geometry.translate(ssx + (xmax-xmin)/spr/2,0,ssz + (zmax-zmin)/spr/2);
+          scene.add( xPlane );
+          xPlanes.push(xPlane);
+        }
+
+    }
+    }
   }
 }
-*/
-
 
 
 //Base Mesh
 
-const material = new THREE.MeshBasicMaterial( { vertexColors: true, side: THREE.DoubleSide } );
-var geom = new THREE.BufferGeometry();
-geom.setAttribute('position' , new THREE.Float32BufferAttribute( vf,3));
-geom.setAttribute('color'    , new THREE.Float32BufferAttribute( cf,3)); 
-const mesh = new THREE.Mesh(geom,material);
+const boardMaterial = new THREE.MeshBasicMaterial( { vertexColors: true, side: THREE.DoubleSide } );
+var boardGeom = new THREE.BufferGeometry();
+boardGeom.setAttribute('position' , new THREE.Float32BufferAttribute( vf,3));
+boardGeom.setAttribute('color'    , new THREE.Float32BufferAttribute( cf,3)); 
+const boardMesh = new THREE.Mesh(boardGeom,boardMaterial);
+//boardMesh.renderOrder = 0;
 
-/*
-const boardGeo = new THREE.PlaneGeometry(20,20);
-const mesh = new THREE.Mesh(boardGeo,material);
-*/
-scene.add( mesh );
-//mesh.rotateX(Math.PI/2);
+scene.add( boardMesh );
 
 //Wire Frame
 /*
@@ -223,34 +202,54 @@ scene.add(wframe);
 */
 
 
+//////////////////////////////////
+// Topological Transformations  //
+//////////////////////////////////
 
+//these transformations depends on the assumption of the plane board being 
+//centered at the origin
 
 function toCylinder(x,y,z) {
   //angle x maps to in cylinder
   // -xr/2 --- x --- xr/2 =>  3PI/2 ----- theta ----- -PI/2
   let theta = (-x/xrange)*2*Math.PI + Math.PI/2;
-  // C = 2*PI*r => r = C/(2*PI)
+
   let radius = (xrange/(2*Math.PI));
-  let res = [ radius*Math.cos(theta),
-              radius*Math.sin(theta),
-              z,
-            ]
+  // C = 2*PI*r => r = C/(2*PI)
+  
+  //experimental
+  //since we wish to use this to map extruded geometry (tiles)
+  //we define a special case in the input space ( y != 0 )
+  //when this is the case, we modify the output vector by displacing it 
+  //that amount in the normal direction
+  
+  //as the planar surface has y = 0 , nonzero y's must be for transforming tiles
+
+  let tanx = -radius*Math.sin(theta);
+  let tany = radius*Math.cos(theta);
+
+  let normalx = tany;
+  let normaly = -1*tanx;
+
+  //normalize normal vector
+  let nnorm = Math.sqrt( normalx**2 +  normaly**2 );
+
+  let res = [];
+  if ( y == 0 ) {
+    res = [ radius*Math.cos(theta),
+                radius*Math.sin(theta),
+                z,
+              ]
+  //extruded geometry
+  } else {
+    res = [     radius*Math.cos(theta) + y*normalx/nnorm,
+                radius*Math.sin(theta) + y*normaly/nnorm,
+                z
+              ]
+  }
   return res;
 }
 
-/*
-function toTorusFromCylinder(x,y,z) {
-  let theta = (-z/zrange)*2*Math.PI + Math.PI/2;
-  let radius = (zrange/(2*Math.PI));
-  //y is no constant
-  //x and z change as they wrap around y axis
-  let res = [ radius*Math.sin(theta),
-              y,
-              radius*Math.cos(theta)
-  ]
-  return res;
-}
-*/
 
 
 function toTorusFromCylinder(x,y,z) {
@@ -281,6 +280,7 @@ function toTorusFromCylinder(x,y,z) {
   let rotTempx = tempx * Math.cos(theta) + tempz * -Math.sin(theta);
   let rotTempz = tempx * Math.sin(theta) + tempz * Math.cos(theta);
 
+  //normal doesn't need to cheat here, not 100% why but it just works
 
   //y is no constant
   //x and z change as they wrap around y axis
@@ -294,60 +294,28 @@ function toTorusFromCylinder(x,y,z) {
 
 
 
-// X - shape construction 
-// i
-/*
-const xShape = new THREE.Shape();
-
-xShape.moveTo( 25, 25 );
-xShape.bezierCurveTo( 25, 25, 20, 0, 0, 0 );
-xShape.bezierCurveTo( - 30, 0, - 30, 35, - 30, 35 );
-xShape.bezierCurveTo( - 30, 55, - 10, 77, 25, 95 );
-xShape.bezierCurveTo( 60, 77, 80, 55, 80, 35 );
-xShape.bezierCurveTo( 80, 35, 80, 0, 50, 0 );
-xShape.bezierCurveTo( 35, 0, 25, 25, 25, 25 );
-
-const extrudeSettings = { depth: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
-
-const xgeometry = new THREE.ExtrudeGeometry( xShape, extrudeSettings );
-
-const xmesh = new THREE.Mesh( xgeometry, new THREE.MeshPhongMaterial() );
-scene.add(xmesh);
-*/
-
-
-// X - temp
-const xGeometry = new THREE.PlaneGeometry(100,100,50);
-const xMat = new THREE.MeshBasicMaterial( { color : 0xffff00 , side : THREE.DoubleSide});
-const xPlane = new THREE.Mesh( xGeometry , xMat );
-xPlane.geometry.rotateX(Math.PI/2);
-scene.add( xPlane );
-
-// O - shape construction 
-/*
-const ringGeometry = new THREE.RingGeometry( 90, 100, 8 );
-const rmat = new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide } );
-const o00 = new THREE.Mesh( ringGeometry, rmat );
-o00.rotateX(-Math.PI/2);
-scene.add( o00 );
-*/
 
 
 
 
 
-const positions = geom.attributes.position.array;
-console.log(positions);
+const positions = boardGeom.attributes.position.array;
 // pos l /3
+
+//Calculate the different states that the board and the peices 
+//can be in and store the vertex data
 
 //store the intial shape
 let S = positions.map((x) => x);
-//calculate final shape
+
+// Cylinder Vertex data
 let SF_PC = positions.map((x) => x);
 
+// Torus Vertex data
 let SF_CT = positions.map((x) => x);
 
-//plane to cylinder
+// Calculate Cylinder vertex data by applying plane
+// to cylinder transformation
 for ( let i = 0; i < positions.length/3; i++) {
   let cyl = toCylinder(S[3*i],S[3*i +1],S[3*i+2]);
   SF_PC[3*i] = cyl[0];
@@ -355,7 +323,8 @@ for ( let i = 0; i < positions.length/3; i++) {
   SF_PC[3*i + 2] = cyl[2];
 }
 
-//cylinder to torus
+// Calculate Torus vertex data by applying cylinder
+// to plane transformation
 for ( let i = 0; i < positions.length/3; i++) {
   let tor = toTorusFromCylinder(SF_PC[3*i],SF_PC[3*i +1],SF_PC[3*i+2]);
   SF_CT[3*i] = tor[0];
@@ -363,25 +332,46 @@ for ( let i = 0; i < positions.length/3; i++) {
   SF_CT[3*i + 2] = tor[2];
 }
 
-console.log(S);
-console.log(SF_PC);
 
-//transformations of o00
-console.log(xPlane.geometry.attributes.position.array);
-let x_S =  xPlane.geometry.attributes.position.array.map((x) => x);
-console.log(xPlane);
+//todo map of maps where
+//each xPlane has a map to each of it's geometric configurations
 
-let x_S_PC = x_S.map((x) => x);
-//O plane -> O cylinder
-for ( let i = 0; i < x_S.length/3; i++) {
-  let cyl = toCylinder(x_S[3*i],x_S[3*i +1],x_S[3*i+2]);
-  x_S_PC[3*i] = cyl[0];
-  x_S_PC[3*i + 1] = cyl[1];
-  x_S_PC[3*i + 2] = cyl[2];
-}
+// X planes geometry cache
+let xPlanesGeoCache = [];
+//calculate each topological state of each X plane peice
+console.log(xPlanes);
+xPlanes.forEach( xp => {
+  const pos = xp.geometry.attributes.position.array;
 
-console.log(x_S_PC);
+  let pl = pos.map((x) => x);
 
+  let cy = pos.map((x) => x);
+  for ( let i = 0; i < pos.length/3; i++) {
+    let cyl = toCylinder(pos[3*i],pos[3*i +1],pos[3*i+2]);
+    cy[3*i] = cyl[0];
+    cy[3*i + 1] = cyl[1];
+    cy[3*i + 2] = cyl[2];
+  }
+
+  let to = pos.map((x) => x);
+  for ( let i = 0; i < pos.length/3; i++) {
+    let tor = toTorusFromCylinder(cy[3*i],cy[3*i +1],cy[3*i+2]);
+    to[3*i] = tor[0];
+    to[3*i + 1] = tor[1];
+    to[3*i + 2] = tor[2];
+  }
+
+  let cache = {
+    mesh     : xp,
+    plane    : pl,
+    cylinder : cy,
+    torus    : to
+  }
+  xPlanesGeoCache.push(cache);
+
+});
+
+//console.log(xPlanesGeoCache);
 
 
 
@@ -389,12 +379,6 @@ console.log(x_S_PC);
 function cylinderTransform(t) {
       //transform all points one time step
 			for ( let i = 0; i < positions.length/3; i++) {
-      
-        /* 
-        let x = positions[3*i];
-        let y = positions[3*i + 1];
-        let z = positions[3*i + 2];
-        */
 
         //find (x,y)'s point in starting image
         let x = S[3*i];
@@ -428,7 +412,54 @@ function cylinderTransform(t) {
           positions[3*i + 1] =  yn;
           positions[3*i + 2] =  zn;
 
+
       }
+
+      xPlanesGeoCache.forEach( xp => {
+
+        for ( let i = 0; i < xp.mesh.geometry.attributes.position.array.length/3; i++) {
+
+          //find (x,y)'s point in starting image
+          let x = xp.plane[3*i];
+          let y = xp.plane[3*i + 1];
+          let z = xp.plane[3*i + 2];
+
+          //find (x,y)'s point in final image SF
+          let xf = xp.cylinder[3*i];
+          let yf = xp.cylinder[3*i + 1];
+          let zf = xp.cylinder[3*i + 2];
+
+          //interpolate a line between x,y,z and xf,yf,zf
+            //find distance vector
+            let xd = xf - x;
+            let yd = yf - y;
+            let zd = zf - z;
+
+            //draw distance vectors for debug perspective
+            /*
+            var xgeo = new THREE.BufferGeometry();
+            var xpos = Float32Array.from([x,y,z,xf,yf,zf]);
+            xgeo.setAttribute('position' , new THREE.BufferAttribute(xpos , 3));
+            var matx = new THREE.LineBasicMaterial( { color: 0xf0fff0 } );
+            var line = new THREE.Line( xgeo , matx );
+            scene.add(line);
+            */
+
+            let xn = x + ( (xd) * t);
+            let yn = y + ( (yd) * t);
+            let zn = z + ( (zd) * t);
+            xp.mesh.geometry.attributes.position.array[3*i]     =  xn;
+            xp.mesh.geometry.attributes.position.array[3*i + 1] =  yn;
+            xp.mesh.geometry.attributes.position.array[3*i + 2] =  zn;
+
+
+        }
+
+        xp.mesh.geometry.attributes.position.needsUpdate = true;
+      });
+
+
+      /*
       //experiment for transforming o00 at the same time
       for ( let i = 0; i < x_S.length/3; i++) {
 
@@ -466,7 +497,7 @@ function cylinderTransform(t) {
       }
 
       xPlane.geometry.attributes.position.needsUpdate = true;
-
+      */
 }
 
 function identity(t) {
@@ -515,45 +546,74 @@ function torusTransform(t) {
           positions[3*i]     =  xn;
           positions[3*i + 1] =  yn;
           positions[3*i + 2] =  zn;
+
       }
+
+
+      xPlanesGeoCache.forEach( xp => {
+
+        for ( let i = 0; i < xp.mesh.geometry.attributes.position.array.length/3; i++) {
+
+          //find (x,y)'s point in starting image
+          let x = xp.cylinder[3*i];
+          let y = xp.cylinder[3*i + 1];
+          let z = xp.cylinder[3*i + 2];
+
+          //find (x,y)'s point in final image SF
+          let xf = xp.torus[3*i];
+          let yf = xp.torus[3*i + 1];
+          let zf = xp.torus[3*i + 2];
+
+          //interpolate a line between x,y,z and xf,yf,zf
+            //find distance vector
+            let xd = xf - x;
+            let yd = yf - y;
+            let zd = zf - z;
+
+            //draw distance vectors for debug perspective
+            /*
+            var xgeo = new THREE.BufferGeometry();
+            var xpos = Float32Array.from([x,y,z,xf,yf,zf]);
+            xgeo.setAttribute('position' , new THREE.BufferAttribute(xpos , 3));
+            var matx = new THREE.LineBasicMaterial( { color: 0xf0fff0 } );
+            var line = new THREE.Line( xgeo , matx );
+            scene.add(line);
+            */
+
+            let xn = x + ( (xd) * t);
+            let yn = y + ( (yd) * t);
+            let zn = z + ( (zd) * t);
+            xp.mesh.geometry.attributes.position.array[3*i]     =  xn;
+            xp.mesh.geometry.attributes.position.array[3*i + 1] =  yn;
+            xp.mesh.geometry.attributes.position.array[3*i + 2] =  zn;
+
+
+        }
+
+        xp.mesh.geometry.attributes.position.needsUpdate = true;
+      });
+
 }
 
 
-let ti = 0;
-let tsteps = 20;
-let lastStep = performance.now();
-let dir = 1;
-let transformCycle = [ 
-  { kind : cylinderTransform , dir: 1 },
-//  { kind : torusTransform    , dir: 1 },
-// { kind : torusTransform    , dir: -1},
-  { kind : cylinderTransform , dir: -1},
-];
-
-
-//let transformCycle = [ { kind : identity , dir: 1} ];
-let transformIndex = 0;
-let transform = identity;
 
 //Ray casting
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
 
 function mouseMove(event) {
-  pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-  pointer.y = ( event.clientY / window.innerHeight) * 2 - 1;
+  pointer.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
+  pointer.y = ( event.clientY / renderer.domElement.clientHeight) * -2 + 1;
+  //console.log(pointer);
 }
 
 window.onmousemove = mouseMove;
 
-function cast() {
-raycaster.setFromCamera( pointer , camera);
-let intersections = raycaster.intersectObjects( scene.children );
-for ( let i = 0; i < intersections.length; i++) {
-  console.log( intersections[i]);
-}
-}
 
+
+///////////////////////
+//Interactation hookup
+///////////////////////
 
 
 window.onkeydown = function (key) {
@@ -568,20 +628,63 @@ window.onkeydown = function (key) {
       transformIndex = (transformIndex + 1) % transformCycle.length;
     }
   }
-  //enter
   if(key.keyCode === 13) {
     console.log("enter hit");
-    cast();
+
+    raycaster.setFromCamera( pointer , camera);
+    let intersections = raycaster.intersectObjects( scene.children );
+    /*
+    for ( let i = 0; i < intersections.length; i++) {
+      console.log( intersections[i]);
+    }
+    */
+    let firstHit = intersections[0];
+    console.log(intersections);
+    console.log(firstHit);
+    if ((firstHit) && firstHit.distance != 0) {
+      /*
+      let sgeo = new THREE.SphereGeometry(30,200,200);
+      sgeo.translate(firstHit.point.x,firstHit.point.y,firstHit.point.z);
+      let smat = new THREE.MeshBasicMaterial({color: 0xff00f0});
+      let sphere = new THREE.Mesh(sgeo,smat);
+      scene.add(sphere);
+      */
+      //firstHit.object.geometry.setAttribute({color : 0xffff00 });
+      firstHit.object.material.color = new THREE.Color( 0xff00ff * Math.random());
+    }
   }
 }
 
+
+
+////////////////////////
+//Transformation Logic//
+////////////////////////
+
+
+let ti = 0;
+let tsteps = 20;
+let lastStep = performance.now();
+let dir = 1;
+let transformCycle = [ 
+  { kind : cylinderTransform , dir: 1 },
+  { kind : torusTransform    , dir: 1 },
+  { kind : torusTransform    , dir: -1},
+  { kind : cylinderTransform , dir: -1},
+];
+
+
+//let transformCycle = [ { kind : identity , dir: 1} ];
+let transformIndex = 0;
+let transform = identity;
+
+
 function animate() {
   requestAnimationFrame( animate );
-
   if ( performance.now() > lastStep + 75 ) {
 
       transform( (dir==1) ? ti/tsteps : 1 - (ti/tsteps) );
-      geom.attributes.position.needsUpdate = true;
+      boardGeom.attributes.position.needsUpdate = true;
       console.log(ti);
       lastStep = performance.now();
 
